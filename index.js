86b4dcfc7ada05dde2c4d2e171c0ff2218b7ca0b
Voilazza <57091579+Voilazza@users.noreply.github.com>
2020-02-07 13:27:27 +0300

    animation function deleted

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-
-$(document).ready(function(){
-    $('.gallery-photo').hover(
-        function() {
-            $(this).addClass('animated flipOutY');
-        },function() {
-            $(".gallery-text").addClass('animated flipInY k');
-        }
-    );
-});
 $(document).ready(function(){
     $('.footer-content').hover(
         function() {
